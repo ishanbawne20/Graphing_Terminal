a8826ddf07f427ef20faff94a634c7a5963f8935
IshanBawne <ujjwal6921@gmail.com>
2021-04-13 15:54:00 +0530

    Corrected Axes and Recentered Camera

--- Graphing_Terminal/main.c
+++ Graphing_Terminal/main.c
@@ -7,7 +7,7 @@
 // Main Variables 
 
 Output output;
-Camera camera = { {-30,screen_height - 20}, 1 };
+Camera camera = { {-screen_width/2,screen_height/2}, 1 };
 
 // Function Declarations
 
@@ -28,31 +28,31 @@ void mainProcess()
 {
 reiterate :
 	
-	printf("");
+	drawAxes(camera);
 
 	//Template for Rendering Curve with one parameter.
 
-	CurveParamOne circle = { 0, 6.5, 0.02 };
+	/*CurveParamOne circle = { 0, 6.5, 0.02 };
 
 	for (float i = circle.one_start; i < circle.one_end; i += circle.one_step)
 	{
-		Vector point = { 30 + 10 * cos(i), 15 + 10 * sin(i) };
+		Vector point = { 20 + 10 * cos(i), 15 + 10 * sin(i) };
 		ScreenVector index = indexer(point, camera);
 		output.stream[index.y][index.x] = index.pixel;
-	}
+	}*/
 
 	// Another Template for rendering on parameter curve
-
-	/*CurveParamOne sincurve = { -20, 50, 0.01 };
+	
+	CurveParamOne sincurve = { -30, 40, 0.01 };
 
 	for (float i = sincurve.one_start; i < sincurve.one_end; i+= sincurve.one_step)
 	{
 		Vector po = { i, 2*sin(i) };
 		ScreenVector indo = indexer(po, camera);
 		output.stream[indo.y][indo.x] = indo.pixel;
-	}*/
+	}
 
-	CurveParamOne Limacon = { 0, 6.5, 0.01 };
+	/*CurveParamOne Limacon = { 0, 6.5, 0.01 };
 
 	float a = 1;
 	float b = 1;
@@ -62,9 +62,8 @@ reiterate :
 		Vector po = { (b + (a*cos(i)))*cos(i), (b + (a * cos(i)))*sin(i) };
 		ScreenVector indo = indexer(po, camera);
 		output.stream[indo.y][indo.x] = indo.pixel;
-	}
-
-	drawAxes(camera);
+	}*/
+	
 	render(output.stream);
 	camera = takeCommand(camera);
 	initialize_cam();
@@ -95,11 +94,11 @@ void drawAxes(Camera camera)
 			{
 				output.stream[j][i] = '+';
 			}
-			if ((0 >= bounds.x_start + (i * step_x)) && (0 < bounds.x_start + ((i + 1) * step_x)))
+			else if ((0 >= bounds.x_start + (i * step_x)) && (0 < bounds.x_start + ((i + 1) * step_x)))
 			{
 				output.stream[j][i] = '|';
 			}
-			if ((0 >= bounds.y_start + (j * step_y)) && (0 < bounds.y_start + ((j + 1) * step_y)))
+			else if ((0 >= bounds.y_start + (j * step_y)) && (0 < bounds.y_start + ((j + 1) * step_y)))
 			{
 				output.stream[j][i] = '-';
 			}
